4497003fcb6ca1be59e8f41387cceb385b350069
Alexandra Cherepania <alexandra.cherepania@gamil.com>
2023-09-30 20:02:24 +0300

    update

--- src/land.js
+++ src/land.js
@@ -47,4 +47,75 @@ const landArr = {
         'en': 'Buy now',
     },
 
+    'book': {
+        'ua': 'Як забронювати?',
+        'en': 'How it is made?',
+    },
+    'booktitle1': {
+        'ua': 'Залишаєте заявку',
+        'en': 'Leave an application',
+    },
+    'booktitle2': {
+        'ua': 'Ми уточнюємо Ваші побажання',
+        'en': 'We clarify your wishes',
+    },
+    'booktitle3': {
+        'ua': 'Підбираємо для Вас варіанти відпочинку',
+        'en': 'We select vacation options for you',
+    },
+    'booktitle4': {
+        'ua': 'Оплата туру',
+        'en': 'Tour payment',
+    },
+    'booktitle5': {
+        'ua': 'Видача документів',
+        'en': 'Issuance of documents',
+    },
+    'booktitle6': {
+        'ua': 'Насолоджуєтеся відпочинком',
+        'en': 'Enjoy your vacation',
+    },
+
+    'booktext1': {
+        'ua': 'Натискаєте та кнопку Купити, або телефонуєте.',
+        'en': 'Press that button Buy now  or call.',
+    },
+
+    'booktext2': {
+        'ua': 'Менеджер у розмові з вами уточнює деталі туру.',
+        'en': 'The manager clarifies the details of the tour in a conversation with you.',
+    },
+    'booktext3': {
+        'ua': 'На основі Ваших пріоритетів, пропонуємо Вам варіанти на вибір.',
+        'en': 'Based on your priorities, we offer you options to choose from.',
+    },
+    'booktext4': {
+        'ua': 'Є можливість оплатити на сайті 24/7, або у нашому офісі.',
+        'en': 'It is possible to pay on the website 24/7 or in our office.',
+    },
+    'booktext5': {
+        'ua': 'Отримуєте вашу путівку, у нас в офісі або відправляємо документи на електронну пошту.',
+        'en': 'You receive your voucher in our office or we send documents to e-mail.',
+    },
+    'booktext6': {
+        'ua': 'Ми працюємо - ви відпочиваєте!',
+        'en': 'We work - you rest!',
+    },
+
+
+    'search': {
+        'ua': 'Пошук туру',
+        'en': 'Hot tours',
+    },
+
+    'constructor': {
+        'ua': 'Конструктор турів',
+        'en': 'Constructor of tours',
+    },
+
+    'constructorel': {
+        'ua': 'Створи свою найкращу подорож',
+        'en': 'Create your best trip'
+    }
+
 }

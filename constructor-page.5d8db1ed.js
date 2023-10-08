988418dd6627d61174116cff0058ac945ac752d8
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2023-10-08 16:33:20 +0000

    Deploying to gh-pages from @ AlexandraCherepania/traveling@8c04f4d0280d1693a3c661c1cde4a92373013f92 🚀

--- constructor-page.5d8db1ed.js
+++ constructor-page.5d8db1ed.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("backdrop__is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.a75a80d7.js.map
+//# sourceMappingURL=constructor-page.5d8db1ed.js.map
